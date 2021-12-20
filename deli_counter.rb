a68a3871c8530c326cf7797de592b3013b1ae5db
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2021-12-20 11:11:26 -0600

    commit tree bb8edd46ce693d0a64721188a9a0770bfb65ce6d

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+require "pry"
+
+def line(list)
+    if list.length > 0
+        new_list = list.map.with_index{ |el, i| "#{i + 1}. #{el}" }
+        message = "The line is currently: " + new_list.join(" ")
+        puts message
+    else
+        puts "The line is currently empty."
+    end
+end
+
+def take_a_number(list, name)
+    list << name
+    puts "Welcome, #{name}. You are number #{list.length} in line."
+end
+
+def now_serving(list)
+    return puts "There is nobody waiting to be served!" if list.length == 0
+    puts "Currently serving #{list.first}."
+    list.shift()
+end
